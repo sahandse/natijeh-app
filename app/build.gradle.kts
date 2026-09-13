2a049375b1f948dce1d7946edb99317c44532914
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-13 09:24:37 +0330

    style: polish team player and league profiles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "ir.natijeh.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 13
-    versionName = "1.9.0"
+    versionCode = 14
+    versionName = "1.10.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
