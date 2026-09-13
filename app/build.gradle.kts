a4cd2c923e4425d7e1c3a2bce8350b3180b504f4
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-13 09:19:36 +0330

    feat: add updates offline cache and match highlights

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "ir.natijeh.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 12
-    versionName = "1.8.1"
+    versionCode = 13
+    versionName = "1.9.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
