58b2b759e790b982c15704dd5420d7072fcb7f39
Sahand Rezvan <sahand.rezvanazar@gmail.com>
2026-09-14 12:14:44 +0330

    chore: prepare v1.13.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
     applicationId = "ir.natijeh.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 16
-    versionName = "1.12.0"
+    versionCode = 17
+    versionName = "1.13.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
